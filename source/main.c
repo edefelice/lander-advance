81011fd1c3cbc133fd654a8615d3ab570663ddd3
Ernesto De Felice <contact@ernestodefelice.com>
2026-06-21 12:49:43 +0200

    feature: render static affine background on BG2 (mode 1)

--- source/main.c
+++ source/main.c
@@ -1,7 +1,23 @@
 #include <tonc.h>
+#include "graphics/placeholder.h"
+
+BG_AFFINE affine_bg;
 
 int main(void) {
-    REG_DISPCNT = DCNT_MODE(3) | DCNT_BG2;
-    memset16(vid_mem, RGB15(31, 0, 0), 240 * 160);
-    while(1);
+    // Load background tiles in CBB0
+    memcpy16(tile8_mem[0], placeholderTiles, placeholderTilesLen / 2);
+    // Load background tilemap in SBB 28
+    memcpy16(se_mem[28], placeholderMap, placeholderMapLen / 2);
+    // Load background palette
+    memcpy16(pal_bg_mem, placeholderPal, placeholderPalLen / 2);
+    // Configure BG2
+    REG_BG2CNT = BG_CBB(0) | BG_SBB(28) | BG_AFF_64x64;
+    // Configure BG Affine 2
+    bg_aff_identity(&affine_bg);
+    REG_BG_AFFINE[2] = affine_bg;
+    // Set affine background (Mode 1, BG2)
+    REG_DISPCNT = DCNT_MODE(1) | DCNT_BG2;
+    while(1) {
+        VBlankIntrWait();
+    }
 }
