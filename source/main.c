d8634f36e4a47b54802ea956999ceb426f7c8e7e
Ernesto De Felice <contact@ernestodefelice.com>
2026-06-22 22:45:41 +0200

    feat(affine-background): map height→scale and angle→rotation on BG2 using placeholder values

--- source/main.c
+++ source/main.c
@@ -2,8 +2,31 @@
 #include "graphics/placeholder.h"
 
 BG_AFFINE affine_bg;
+AFF_SRC_EX affine_src;
+
+// Calculates shrinking factor
+// Input: min value, max value, current height, maximum height
+// Output: shrinking factor
+static inline int shrink(const int min, const int max, const int height, const int max_height) {
+    return (height >= 0 && height <= max_height) ? min + (max - min) * height / max_height : max;
+}
 
 int main(void) {
+    // Initialize source affine matrix
+    affine_src.tex_x = 256<<8;
+    affine_src.tex_y = 256<<8;
+    affine_src.scr_x = 240 / 2;
+    affine_src.scr_y = 160 / 2;
+    affine_src.sx = 0x80;
+    affine_src.sy = 0x80;
+    affine_src.alpha = 0;
+    // Parameters to test background scaling
+    int height = 0;
+    int direction = 1;
+    //int height = max_height;
+    int min_shrink = 0x80;
+    int max_shrink = 0x180;
+    int max_height = 1024;
     // Load background tiles in CBB0
     memcpy16(tile8_mem[0], placeholderTiles, placeholderTilesLen / 2);
     // Load background tilemap in SBB 28
@@ -12,12 +35,25 @@ int main(void) {
     memcpy16(pal_bg_mem, placeholderPal, placeholderPalLen / 2);
     // Configure BG2
     REG_BG2CNT = BG_CBB(0) | BG_SBB(28) | BG_AFF_64x64;
-    // Configure BG Affine 2
-    bg_aff_identity(&affine_bg);
-    REG_BG_AFFINE[2] = affine_bg;
     // Set affine background (Mode 1, BG2)
     REG_DISPCNT = DCNT_MODE(1) | DCNT_BG2;
+    irq_init(NULL);
+    irq_add(II_VBLANK, NULL);
     while(1) {
         VBlankIntrWait();
+        height += direction;
+        if (height >= max_height) {
+            direction = -1;
+        }
+        else if (height <= 0) {
+            height = 0;
+            direction = 1;
+        }
+        affine_src.sx = shrink(min_shrink, max_shrink, height, max_height);
+        affine_src.sy = affine_src.sx;
+        affine_src.alpha += 0x100;
+        // Configure BG Affine 2
+        bg_rotscale_ex(&affine_bg, &affine_src);
+        REG_BG_AFFINE[2] = affine_bg;
     }
 }
