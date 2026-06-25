36eb8c4a50544fad0af5be96fc632970acd9ce6c
Ernesto De Felice <contact@ernestodefelice.com>
2026-06-25 23:00:23 +0200

    chore: format enum names using PascalCase

--- include/game_result.h
+++ include/game_result.h
@@ -2,17 +2,17 @@
 #ifndef GAME_RESULT_H
 #define GAME_RESULT_H
 
-typedef enum { GR_WIN = 0, GR_LOSE} gr_outcome;
+typedef enum { GR_WIN = 0, GR_LOSE} GrOutcome;
 
 typedef enum {
     GR_REASON_TOO_FAST = 0,
     GR_REASON_OUT_OF_PAD
-} gr_reason;
+} GrReason;
 
 typedef struct {
-    gr_outcome outcome;
-    gr_reason  reason;
+    GrOutcome outcome;
+    GrReason  reason;
     int        score;
-} game_result;
+} GameResult;
 
 #endif // GAME_RESULT_H
