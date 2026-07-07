b1a96480b786d46aae41f8686fcd1f2935f5b1d7
Liode01 <pierlux01@gmail.com>
2026-07-07 20:04:44 +0200

    Update cockpit.c and .h
    corrected added menu function

--- source/cockpit.c
+++ source/cockpit.c
@@ -4,50 +4,77 @@ u16 cpit_input()
 {
 u16 action = 0;
 
-
-VBlankIntrWait(); 
-key_poll(); 
+VBlankIntrWait();   // To remove if already used outside
+key_poll();         // To remove if already used outside
 
 if(key_is_down(KEY_A)){
-    action ^ MAINT_THR_ON;
+    action = action | MAINT_THR_ON;
 }
 if(key_is_down(KEY_UP)){
-    action ^ TOP_THR_ON;
+    action = action | TOP_THR_ON;
 }
 
 if(key_is_down(KEY_LEFT)){
-    action ^ LEFT_THR_ON;
+    action = action | LEFT_THR_ON;
 }
 
 if(key_is_down(KEY_DOWN)){
-    action ^ BOT_THR_ON;
+    action = action | BOT_THR_ON;
 }
 if(key_is_down(KEY_RIGHT)){
-    action ^ RIGHT_THR_ON;
+    action = action | RIGHT_THR_ON;
 }
 
 if(key_hit(KEY_R) && key_held(KEY_B)){
-    action ^ RADAR_ON;
+    action = action | RADAR_ON;
 } else if (key_is_down(KEY_R)){
-    action ^ L_ROT_THR_ON;
+    action = action | L_ROT_THR_ON;
 }
 
-if(key_hit(KEY_R) && key_held(KEY_B)){
-    action ^ RADAR_ON;
+if(key_hit(KEY_L) && key_held(KEY_B)){
+    action = action | RADAR_ON;
 } else if (key_is_down(KEY_L)){
-    action ^ L_ROT_THR_ON;
+    action = action | L_ROT_THR_ON;
 }
 
+if(key_hit(KEY_START)){
+    action = action | PAUSE;
+}
 
+return action;
 }
 
 u16 menu_input()
 {
-u16 actions;
+u16 action = 0;
 
-VBlankIntrWait();
-key_poll();
+VBlankIntrWait();   // To remove if already used outside
+key_poll();         // To remove if already used outside
 
+if(key_hit(KEY_A)){
+    action = action | M_CONFIRM;
+}
+if(key_hit(KEY_B)){
+    action = action | M_RETURN;
+}
+if(key_hit(KEY_UP)){
+    action = action | M_UP;
+}
 
+if(key_hit(KEY_LEFT)){
+    action = action | M_LEFT;
+}
 
+if(key_hit(KEY_DOWN)){
+    action = action | M_DOWN;
+}
+if(key_hit(KEY_RIGHT)){
+    action = action | M_RIGHT;
+}
+
+if(key_hit(KEY_START)){
+    action = action | PAUSE;
+}
+
+return action;
 }
--- source/cockpit.h
+++ source/cockpit.h
@@ -1,7 +1,38 @@
 #ifndef COCKPIT_H
 #define COCKPIT_H
 
+#include <string.h>
 
+#include <tonc.h>
+
+// Definitions for Gameplay actions
+
+#define MAINT_THR_ON    0x001  
+#define TOP_THR_ON      0x002   // Lateral thruster top (towards down) ON
+#define LEFT_THR_ON     0x004   // Lateral thruster left (towards right) ON 
+#define BOT_THR_ON      0x008   // Lateral thruster bottom (towards up) ON 
+#define RIGHT_THR_ON    0x010   // Lateral thruster right (towards left) ON 
+#define R_ROT_THR_ON    0x020   // Rotation thruster right (counter-clockwise) ON 
+#define L_ROT_THR_ON    0x040   // Rotation thruster left (clockwise) ON
+#define RADAR_ON        0x080 
+#define LIGHT_ON        0x100   
+ 
+
+// Definitions for Menus actions
+
+#define M_CONFIRM       0x001  
+#define M_RETURN        0x002
+#define M_UP            0x004   
+#define M_LEFT          0x008    
+#define M_DOWN          0x010   
+#define M_RIGHT         0x020   
+ 
+// Definitions for General
+
+#define PAUSE           0x200 
+
+u16 cpit_input(void);
+u16 menu_input(void);
 
 
 #endif
