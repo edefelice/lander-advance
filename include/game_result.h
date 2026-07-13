f761dffe8cf106ef2dc1b78e93cc58b4b962712e
Ernesto De Felice <contact@ernestodefelice.com>
2026-07-13 12:55:26 +0200

    fix: add GR_REASON_NONE for winning outcome

--- include/game_result.h
+++ include/game_result.h
@@ -1,11 +1,15 @@
-// Shared struct to store game score.
+// Shared struct to store the outcome of a completed game.
 #ifndef GAME_RESULT_H
 #define GAME_RESULT_H
 
-typedef enum { GR_WIN = 0, GR_LOSE} GrOutcome;
+typedef enum {
+    GR_WIN = 0,
+    GR_LOSE
+} GrOutcome;
 
 typedef enum {
-    GR_REASON_TOO_FAST = 0,
+    GR_REASON_NONE = 0,
+    GR_REASON_TOO_FAST,
     GR_REASON_OUT_OF_PAD
 } GrReason;
 
