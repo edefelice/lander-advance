d135375ea1857e123fe2964e29a4e271ab54dc94
Raffas94 <raffale.auc@gmail.com>
2026-07-19 01:29:41 +0200

    update comments physics constants

--- include/physics_constants.h
+++ include/physics_constants.h
@@ -36,7 +36,7 @@
 
 //mass
 #define DRY_MASS            FIX_FROM_INT(6713)                  //Dry mass 6713 Kg
-#define PROP_MASS           FIX_FROM_INT(4243)                  //Propellant mass 8487 Kg  !!!!!!!!!!!!!!!!!!!!!!STO RIDUCENDO DIVIDENDO PER META'!!!!!!!!!!!!!!
+#define PROP_MASS           FIX_FROM_INT(4243)                  //Propellant mass 4243 Kg  (realistic mass 8487 / 2)
 #define CREW_MASS           FIX_FROM_INT(160)                   // Crew mass (2 astronauts, 80 kg each)
 
 //mass flow main and RCS (Consumption)
