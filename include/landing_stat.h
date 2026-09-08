aee7f0e4659681ed52a85d6965e9cfd2bae47dcb
Liode01 <pierlux01@gmail.com>
2026-09-09 00:32:52 +0200

    added landing zone verification
    added function for verifying if the lander is on the landing area after touching the ground

---
+++ include/landing_stat.h
@@ -0,0 +1,12 @@
+#ifndef LANDING_STAT_H  
+#define LANDING_STAT_H 
+
+#include "gameplay.h"
+#include "landing_area.h"
+
+
+
+
+// Insert Function for map selection
+
+bool IsonPad(const Lander *lander,const int *area, const Sites *moon_sites);
---
+++ source/landing_stat.c
@@ -0,0 +1,11 @@
+#include "landing_stat.h"
+#include "gameplay.h"
+#include "fixedpoint32.h"
+#include "physics_constants.h"
+#include "tonc_math.h"
+#include "tonc_types.h"
+
+
+bool IsonPad(const Lander *lander,const int *area, const Sites *moon_sites){
+    
+}
